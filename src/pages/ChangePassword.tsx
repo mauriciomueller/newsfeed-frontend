47add5e73de339a22e5f1a8350b96277d4343fdd
mauriciomueller <mauriciomueller@gmail.com>
2023-02-22 14:59:39 -0300

    Change password refactor

--- src/pages/ChangePassword.tsx
+++ src/pages/ChangePassword.tsx
@@ -1,88 +1,91 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
+import { Button, Form } from "react-bootstrap"
 import AlertMessageComponent from "../components/AlertMessage"
 import SuccessMessageComponent from "../components/SuccessMessage"
 import { useAuthContext } from "../context/AuthContext"
 
 const ChangePassword = () => {
-	const [old_password, setOldPassword] = useState<string>("")
-	const [new_password, setNewPassword] = useState<string>("")
-	const [new_password_confirmation, setNewPasswordConfirmation] = useState<string>("")
+	const oldPasswordRef = useRef<HTMLInputElement | null>(null)
+	const newPasswordRef = useRef<HTMLInputElement | null>(null)
+	const newPasswordConfirmationdRef = useRef<HTMLInputElement | null>(null)
 
 	const { updatePassword, errors, successMessage } = useAuthContext()
 
 	useEffect(() => {
 		if(successMessage !== '') {
-			setOldPassword('')
-			setNewPassword('')
-			setNewPasswordConfirmation('')
+			oldPasswordRef.current = null
+			newPasswordRef.current = null
+			newPasswordConfirmationdRef.current = null
 		}
 	},[successMessage])
 
 	const handleUpdatePassword = async (event: React.SyntheticEvent) => {
 		event.preventDefault()
 
+		const old_password = oldPasswordRef.current?.value || ''
+		const new_password = newPasswordRef.current?.value || ''
+		const new_password_confirmation = newPasswordConfirmationdRef.current?.value || ''
+
 		updatePassword({ old_password, new_password, new_password_confirmation })
 	}
 
 	return (
 		<section className="mt-5 form-register form-guest w-100 m-auto">
-			<form className="mb-4 needs-validation" onSubmit={handleUpdatePassword} noValidate>
-				<h1 className="h3 mb-3 fw-normal text-center">Change your Password</h1>
 
-				<SuccessMessageComponent successMessage={successMessage} />
-				<AlertMessageComponent alertMessage={errors?.alert} />
+<h1 className="h3 mb-3 fw-normal text-center">Change your Password</h1>
 
-				<div className="form-floating mb-2">
-					<input
+			<SuccessMessageComponent successMessage={successMessage} />
+			<AlertMessageComponent alertMessage={errors?.alert} />
+
+			<Form className="mb-4 needs-validation" onSubmit={handleUpdatePassword} noValidate>
+
+				<Form.Group className="form-floating has-validation mb-2">
+					<Form.Control
+						required
 						type="password"
-						value={old_password}
-						onChange={(e) => setOldPassword(e.target.value)}
-						placeholder="Old Password"
-						className={`form-control ${errors?.old_password ? "is-invalid" : ""}`}
+						placeholder="Old password"
+						autoComplete="password"
+						ref={oldPasswordRef}
+						isInvalid={!!errors?.old_password}
 					/>
-					{errors?.old_password && (
-						<div className="invalid-feedback mb-3">
-							{errors?.old_password[0]}
-						</div>
-					)}
-					<label>Old Password</label>
-				</div>
+					<Form.Label>Old password</Form.Label>
+					<Form.Control.Feedback type="invalid">
+						{errors?.old_password && errors.old_password[0]}
+					</Form.Control.Feedback>
+				</Form.Group>
 
-				<div className="form-floating mb-2">
-					<input
+
+				<Form.Group className="form-floating has-validation mb-2">
+					<Form.Control
+						required
 						type="password"
-						value={new_password}
-						onChange={(e) => setNewPassword(e.target.value)}
-						placeholder="New Password"
-						className={`form-control ${errors?.new_password ? "is-invalid" : ""}`}
+						placeholder="New password"
+						ref={newPasswordRef}
+						isInvalid={!!errors?.new_password}
 					/>
-					{errors?.new_password && (
-						<div className="invalid-feedback mb-3">
-							{errors.new_password[0]}
-						</div>
-					)}
-					<label>New Password</label>
-				</div>
+					<Form.Label>New password</Form.Label>
+					<Form.Control.Feedback type="invalid">
+						{errors?.new_password && errors.new_password[0]}
+					</Form.Control.Feedback>
+				</Form.Group>
 
-				<div className="form-floating mb-2">
-					<input
+
+				<Form.Group className="form-floating has-validation mb-2">
+					<Form.Control
+						required
 						type="password"
-						value={new_password_confirmation}
-						onChange={(e) => setNewPasswordConfirmation(e.target.value)}
-						placeholder="New password confirmation"
-						className={`form-control ${errors?.new_password_confirmation ? "is-invalid" : ""}`}
+						placeholder="New password Confirmation"
+						ref={newPasswordConfirmationdRef}
+						isInvalid={!!errors?.new_password_confirmation}
 					/>
-					{errors?.new_password_confirmation && (
-						<div className="invalid-feedback mb-3">
-							{errors?.new_password_confirmation[0]}
-						</div>
-					)}
-					<label>New password confirmation</label>
-				</div>
+					<Form.Label>New password Confirmation</Form.Label>
+					<Form.Control.Feedback type="invalid">
+						{errors?.new_password_confirmation && errors.new_password_confirmation[0]}
+					</Form.Control.Feedback>
+				</Form.Group>
 
-				<button className="w-100 btn btn-lg btn-primary" type="submit">Change password</button>
-
-			</form>
+				<Button variant="primary" className="w-100 btn-lg" type="submit">Change password</Button>
+			</Form>
 		</section>
 	)
 }
